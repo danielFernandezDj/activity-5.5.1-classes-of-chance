7fae0df778417ba170b421b1603a6106e3aa0277
danielFernandezJd <mdanif9728@gmail.com>
2024-03-21 13:09:30 -0700

    saving

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 class Casino {
-    // Write code here
+    constructor(name) {
+        this.name = name
+        this.timesPlayed = 0
+        this.isFakeCoin = false
+    }
+
+    playGame(betAmount) {
+        this.timesPlayed++
+
+        if (this.isFakeCoin || Math.random() <= 0.5) {
+            console.log(`${this.name} wins!`)
+        } else {
+            const amountWon = betAmount * (this.timesPlayed + 1)
+            console.log(`The Player wins the bet amount!`)
+        }
+    }
 };
 
+
+
 // TESTS
-const myCasino = new Casino("HackerU Casino");
+const myCasino = new Casino("HackerU");
 console.log(myCasino);
 myCasino.playGame(5);
 myCasino.playGame(15);
